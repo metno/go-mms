f7ee1e3189aabae50ecd712adb5ce8f6177d0b60
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-31 15:09:24 +0200

    Fixed the interface function. It now passes the string to and from Python.

--- cmd/mms/export.go
+++ cmd/mms/export.go
@@ -1,11 +1,15 @@
 package main
 
 import "C"
+import "fmt"
 
 //PyPostEvent : Interface function for Python only dealing with JSON strings
 //export PyPostEvent
-func PyPostEvent(msg string) string {
-	return "{\"err\": False, \"errmsg\": \"\"}"
+func PyPostEvent(cMsg *C.char) *C.char {
+	msg := C.GoString(cMsg)
+	fmt.Println("Go received message:")
+	fmt.Println(msg)
+	return C.CString("{\"err\": false, \"errmsg\": \"\"}")
 }
 
 //PyHello : Simple function to check the interface is working.
