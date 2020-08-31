3fe85585afe47da1c3530a9bc1147bf9036326e6
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-31 14:15:16 +0200

    Added interface test function hooked into by pytest

--- cmd/mms/export.go
+++ cmd/mms/export.go
@@ -2,7 +2,14 @@ package main
 
 import "C"
 
+//PyPostEvent : Interface function for Python only dealing with JSON strings
 //export PyPostEvent
 func PyPostEvent(msg string) string {
 	return "{\"err\": False, \"errmsg\": \"\"}"
+}
+
+//PyHello : Simple function to check the interface is working.
+//export PyHello
+func PyHello(x int) int {
+	return 2 * x
 }
