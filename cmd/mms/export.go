22ea9acd3b5e718c0eb88824d09e388dd3b26934
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-09-01 16:05:38 +0200

    Consistent naming

--- cmd/mms/export.go
+++ cmd/mms/export.go
@@ -3,18 +3,18 @@ package main
 import "C"
 import "fmt"
 
-//PyPostEvent : Interface function for Python only dealing with JSON strings
-//export PyPostEvent
-func PyPostEvent(cMsg *C.char) *C.char {
+//PyProductEvent : Interface function for Python only dealing with JSON strings
+//export PyProductEvent
+func PyProductEvent(cMsg *C.char) *C.char {
 	msg := C.GoString(cMsg)
 	fmt.Println("Go received message:")
 	fmt.Println(msg)
 	return C.CString("{\"err\": false, \"errmsg\": \"\"}")
 }
 
-//SayHello : Simple function to check the interface is working.
-//export SayHello
-func SayHello(cMsg *C.char) *C.char {
+//PySayHello : Simple function to check the interface is working.
+//export PySayHello
+func PySayHello(cMsg *C.char) *C.char {
 	msg := C.GoString(cMsg)
 	fmt.Printf("Python says: %s\n", msg)
 	return C.CString("Hello Python!")
