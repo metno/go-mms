de06b013099291956c7c3956ad8e50ea421813a5
Håvard Futsæter <havardf@met.no>
2020-09-07 15:29:55 +0200

    Start NATS server, name NATS server after the production hub it belongs to.

--- cmd/mmsd/main.go
+++ cmd/mmsd/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/metno/go-mms/internal/api"
+	nats "github.com/nats-io/nats-server/v2/server"
 )
 
 const staticFilesDir = "./static/"
+const productionHubName = "default"
 
 func main() {
 	templates := template.Must(template.ParseGlob("templates/*"))
 
 	service := api.NewService(templates, staticFilesDir)
 
-	log.Println("Starting webserver...")
+	log.Println("Starting webserver for internal services ...")
 	go func() {
 		http.ListenAndServe(":8088", service.InternalRouter)
 	}()
@@ -27,5 +30,25 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 		IdleTimeout:  10 * time.Second,
 	}
+	startNATSServer()
+
+	log.Println("Starting webserver ...")
 	log.Fatal(server.ListenAndServe())
+}
+
+func startNATSServer() {
+	s, err := nats.NewServer(&nats.Options{
+		ServerName: fmt.Sprintf("mmsd-nats-server-%s", productionHubName),
+	})
+	if err != nil {
+		nats.PrintAndDie(fmt.Sprintf("nats server failed: %s for server: mmsd-nats-server-%s", err, productionHubName))
+	}
+
+	go func() {
+		log.Println("Starting NATS server...")
+		if err := nats.Run(s); err != nil {
+			nats.PrintAndDie(err.Error())
+		}
+		s.WaitForShutdown()
+	}()
 }
--- go.mod
+++ go.mod
@@ -6,8 +6,9 @@ require (
 	github.com/cloudevents/sdk-go/protocol/nats/v2 v2.2.0
 	github.com/cloudevents/sdk-go/v2 v2.2.0
 	github.com/google/uuid v1.1.1
-	github.com/urfave/cli/v2 v2.2.0
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
+	github.com/nats-io/nats-server/v2 v2.1.8
 	github.com/prometheus/client_golang v1.4.1
+	github.com/urfave/cli/v2 v2.2.0
 )
