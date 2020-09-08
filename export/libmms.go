2e5da89dbf3187b88182d1ca5679ee89cbabb815
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-09-08 13:59:09 +0200

    Added code to create production event in Python interface

--- export/libmms.go
+++ export/libmms.go
@@ -2,7 +2,11 @@ package main
 
 import "C"
 import (
+	"encoding/json"
 	"fmt"
+	"time"
+
+	"github.com/metno/go-mms/pkg/mms"
 )
 
 //PyProductEvent : Interface function for Python only dealing with JSON strings
@@ -11,7 +15,18 @@ func PyProductEvent(cMsg *C.char) *C.char {
 	msg := C.GoString(cMsg)
 	fmt.Println("Go received message:")
 	fmt.Println(msg)
-	return C.CString("{\"err\": false, \"errmsg\": \"\"}")
+
+	var productEvent mms.ProductEvent
+	json.Unmarshal([]byte(msg), &productEvent)
+	productEvent.CreatedAt = time.Now()
+
+	hubs := mms.ListProductionHubs()
+	err := mms.MakeProductEvent(hubs, &productEvent)
+	if err != nil {
+		return C.CString(fmt.Sprintf("{\"err\": true, \"errmsg\": \"%s\"}", err.Error()))
+	} else {
+		return C.CString("{\"err\": false, \"errmsg\": \"\"}")
+	}
 }
 
 //PySayHello : Simple function to check the interface is working.
