11d2668a0892dea833f6014a3d13db191e947254
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-03-09 18:51:47 +0100

    Fix broken test

--- pkg/mms/events_test.go
+++ pkg/mms/events_test.go
@@ -46,21 +46,13 @@ var erroneousEventData = `
 }`
 
 var correctEventData = `
-{
-	"data": {
+[{
 	"Product": "Arome Arctic",
 	"ProductionHub": "ecflow.modellprod",
 	"Product": "arome_arctic_sfx_2_5km",
 	"CreatedAt": "2020-08-26T12:18:48.281847242+02:00",
 	"ProductLocation": ""
-	},
-	"datacontenttype": "application/json",
-	"id": "0173c5ce-e1fb-11ea-9c78-6b708419aa07",
-	"source": "ecflow/modellprod",
-	"specversion": "1.0",
-	"subject": "arome.arctic",
-	"type": "no.met.Product.created.v1"
-}`
+}]`
 
 func TestProductEvent(t *testing.T) {
 	eventData := ProductEvent{}
